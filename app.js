f0df4fce8a3455da5516d7d652cf439f88d3c702
JordanDev <jordandev@pop-os.localdomain>
2020-08-06 18:09:48 -0500

    Co-authored-by: JGurus <ljgurumendi@gmail.com>

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
+//Variables
 const button = document.getElementById("button");
-button.addEventListener("click", pulsacion);
 const app = document.getElementById("root");
 const input = document.getElementById("buscar");
 const spinner = document.getElementById("spinner");
+const pagina = document.getElementById("pagina");
 let palabra = "";
+
+//Eventos
+button.addEventListener("click", pulsacion);
 
 input.addEventListener("keyup", (e) => {
   palabra = e.target.value;
   console.log(palabra);
 });
 
+document.body.addEventListener("click", verMas);
+
+//Funciones
+
 async function pulsacion(e) {
   app.innerHTML = "";
-
   e.preventDefault();
   if (!palabra.trim()) {
     return;
   }
-
   if (palabra.includes(" ")) {
-    palabra.replace(" ", "_");
+    palabra.replace(" ", "%20");
   }
   button.classList.add("animate__heartBeat");
-
   spinner.style.display = "block";
   setTimeout(async () => {
     const uri = `https://es.wikipedia.org/w/api.php?action=query&list=search&srprop=snippet&format=json&origin=*&utf8=&srsearch=${palabra}`;
@@ -31,19 +36,46 @@ async function pulsacion(e) {
     button.classList.remove("animate__heartBeat");
     spinner.style.display = "none";
     const data = res.query.search;
-    data.forEach((art) => {
-      const div = document.createElement("div");
-      const titulo = document.createElement("h2");
-      const parrafo = document.createElement("p");
-      div.classList = "animate__animated animate__backInLeft";
-
-      titulo.innerHTML = art.title;
-      parrafo.innerHTML = art.snippet;
-      div.appendChild(parrafo);
-      div.insertBefore(titulo, parrafo);
-      app.appendChild(div);
-    });
-  }, 2000);
+    recorrer(data);
+  }, 1000);
 }
 
-console.log("hola");
+async function verMas(e) {
+  e.preventDefault();
+  if (e.target.id === "verMas") {
+    pagina.innerHTML = "";
+    const titleArt = e.target.previousElementSibling.previousElementSibling.id;
+    if (titleArt.includes(" ")) {
+      titleArt.replace(" ", "_");
+    }
+    window.scrollTo(0, app.clientHeight);
+    spinner.style.display = "block";
+    window.scrollTo();
+
+    const res = await fetch(
+      `https://es.wikipedia.org/api/rest_v1/page/html/${titleArt}`
+    );
+    const text = await res.text();
+    spinner.style.display = "none";
+    pagina.innerHTML = text;
+  }
+}
+
+const recorrer = (data) => {
+  data.forEach((art) => {
+    const div = document.createElement("div");
+    const titulo = document.createElement("h2");
+    titulo.id = art.title;
+    const parrafo = document.createElement("p");
+    const buttonVer = document.createElement("button");
+    buttonVer.innerText = "Ver mas";
+    buttonVer.id = "verMas";
+    div.classList = "animate__animated animate__backInLeft";
+    titulo.innerHTML = art.title;
+    parrafo.innerHTML = art.snippet;
+    div.appendChild(buttonVer);
+    div.insertBefore(parrafo, buttonVer);
+    div.insertBefore(titulo, parrafo);
+    app.appendChild(div);
+  });
+};
